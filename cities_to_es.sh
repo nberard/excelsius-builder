41c957119a71f803321b47b79e947d9da7544f27
Nicolas BERARD <n.berard@meetic-corp.com>
2016-03-11 11:50:03 +0100

    feat: dynamically get city list

--- cities_to_es.sh
+++ cities_to_es.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget http://bulk.openweathermap.org/sample/city.list.json.gz && gunzip city.list.json.gz
 sed ':a;N;$!ba;s/\n/\n{"index": {}}\n/g' city.list.json | sed 's/_id/internal_id/g' | sed '1 i\{"index": {}}' > city.list.es.json
